b606b9f96c5e583645a12799212f526c4c402393
Vaibhav Gupta <vaibhav.gupta@innovationm.com>
2021-11-23 17:05:58 +0530

    ff

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize('auth-db', 'postgres', 'naimu', {
+module.exports = new Sequelize('auth_db', 'postgres', 'vibhu@224', {
   host: 'localhost',
   dialect: 'postgres'
 })
