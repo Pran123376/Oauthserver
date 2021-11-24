8a00f6d2f82ad56f7bba48ea10bafd7b93793f74
Naimuddin <naimuddin.saifi@innovationm.com>
2021-11-24 17:26:57 +0530

    add env

--- db/db.js
+++ db/db.js
@@ -1,7 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize('auth-db', 'postgres', 'naimu', {
-  host: 'localhost',
+const dbName = process.env.DB_NAME || 'auth-db'
+const dbUser = process.env.DB_USER || 'postgres'
+const dbPassword = process.env.DB_PASS || 'naimu'
+const dbHost = process.env.DB || 'localhost'
+
+module.exports = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
   dialect: 'postgres'
 })
+
+
 
