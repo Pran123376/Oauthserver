d83538c46bb42fee0577e91c60ea5e05d5488c8d
vaibhav <naimuddin.saifi@innovationm.com>
2021-11-26 09:08:32 +0530

    new

--- db/db.js
+++ db/db.js
@@ -1,17 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-<<<<<<< HEAD
-module.exports = new Sequelize('auth_db', 'postgres', 'vibhu@224', {
-  host: 'localhost',
-=======
-const dbName = process.env.DB_NAME || 'auth-db'
+
+const dbName = process.env.DB_NAME || 'auth_db'
 const dbUser = process.env.DB_USER || 'postgres'
-const dbPassword = process.env.DB_PASS || 'naimu'
+const dbPassword = process.env.DB_PASS || 'vibhu@224'
 const dbHost = process.env.DB || 'localhost'
 
 module.exports = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
->>>>>>> 8a00f6d2f82ad56f7bba48ea10bafd7b93793f74
   dialect: 'postgres'
 })
 
